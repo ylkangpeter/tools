1d5509e8553a54ce226532c31a4cca471f447c73
peter <ylkangpeter@gmail.com>
2014-08-07 18:08:50 +0800

    fix

---
+++ formula/com/peter/utils/FormulaException.java
@@ -0,0 +1,10 @@
+package com.peter.utils.formula;
+
+public class FormulaException extends Exception {
+
+	private static final long serialVersionUID = -959109194504948106L;
+
+	public FormulaException(String msg) {
+		super(msg);
+	}
+}
